16ba554a4b5a47c85a620daf67a8ebf0d2185821
khalid Elabd <113540126+khalidKE@users.noreply.github.com>
2024-08-16 13:25:04 +0200

    Update ex1AreaOfCircle.java

--- java/ex1AreaOfCircle.java
+++ java/ex1AreaOfCircle.java
@@ -1,13 +1,21 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
-        int length ,width ,area;
-        Scanner cin =new Scanner(System.in);
-        System.out.print("Enter the length : ");
-        length =cin.nextInt();
-        System.out.print("Enter the width : ");
-        width=cin.nextInt();
-        area=length*width;
-        System.out.println("the area of circle is : "+area);
+        // calc Area of rectangle
+        // int length ,width ,area;
+        // Scanner cin =new Scanner(System.in);
+        // System.out.print("Enter the length : ");
+        // length =cin.nextInt();
+        // System.out.print("Enter the width : ");
+        // width=cin.nextInt();
+        // area=length*width;
+        // System.out.println("the area of circle is : "+area);
+        
+        // clac area of circle 
+        Scanner input =new Scanner(System.in);
+        System.out.println("Enter the radius ,plz");
+        int r;
+        r=input.nextInt();
+        System.out.print("the area of circle is : "+r*r*3.14); 
     }
 }
