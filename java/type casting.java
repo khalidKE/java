1d11fd37319c6b7999e99b73ae2701e166192112
khalid Elabd <113540126+khalidKE@users.noreply.github.com>
2024-08-18 00:43:16 +0200

    Add files via upload

---
+++ java/type casting.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+        int a=3,b=2;
+       System.out.println(a/(float)b); // type casting with variables
+       System.out.println(3/(float)2);
+        System.out.println(3/2f);
+        System.out.println(3/2.0);
+        // in all result same output 1.5
+
+        /************************
+        
+        //pre-increment ++var
+        //post-increment var++
+
+
+        //pre-decrement --var
+        //post=decrement var--
+
+        *************************/
+    }
+}
